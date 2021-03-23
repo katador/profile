03d868952802ba9ffc02cb2b984ea092e07e6c4f
marco antonio bohorquez <antoniob@overskull.pe>
2021-03-22 19:39:39 -0500

    inicio

--- App/scrap.js
+++ App/scrap.js
@@ -4,8 +4,16 @@ btnscrap.addEventListener('click', async ()=>{
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     var port = chrome.tabs.connect(tab.id);
-    port.postMessage({acction: 'scraping'});
+    var serachText = document.getElementById('searchText').value;
+  
+    if(serachText){
+        document.getElementById('loader').style.display = "block";
+        port.postMessage({acction: 'search', searchtext:serachText});
+    }
+   
 })
+
+
 
 
 
--- app.js
+++ app.js
@@ -1,209 +1,273 @@
-const scrapingProfile = async ()=>{
-    //Utils
+const searchProfile = async (textSearch)=>{
+
+    console.log('history',chrome.history);
+
+    const selectorPage = {
+        btnSearch:{
+            text:textSearch,
+            input:"#global-nav-typeahead input",
+        },
+        listSearch:{
+            list:'.search-results-container ul>li',
+            link:'.linked-area',
+        }
+    }
+    /* Utils Timer */
     const wait = (milliseconds)=>{
         return new Promise(function(resolve){
-            setTimeout(function() {
+            setTimeout(function(){
                 resolve();
-            }, milliseconds);
-        });
-    };
-
-    const autoscrollToElement = async function(cssSelector){
-
-        var exists = document.querySelector(cssSelector);
-    
-        while(exists){
-            //
-            let maxScrollTop = document.body.clientHeight - window.innerHeight;
-            let elementScrollTop = document.querySelector(cssSelector).offsetHeight
-            let currentScrollTop = window.scrollY
-    
-    
-            if(maxScrollTop == currentScrollTop || elementScrollTop <= currentScrollTop)
-                break;
-    
-            await wait(32)
-    
-            let newScrollTop = Math.min(currentScrollTop + 20, maxScrollTop);
-            window.scrollTo(0,newScrollTop)
+            },milliseconds)
+        })
+    }
+    /* Utils selector List */
+    const selectorListSearchResult = async (index)=>{
+        const keywords = location.href
+        const arreglo = keywords.split('/search/results/all/');
+        arreglo.length
+        if(arreglo.length < 2){
+            await getSearch()
+            await wait(3000)
         }
-    
-        console.log('finish autoscroll to element %s', cssSelector);
-    
-        return new Promise(function(resolve){
-            resolve();
-        });
-    };
+        const {listSearch:search} = selectorPage
+        const resultList = document.querySelectorAll(search.list)
+        return (index == undefined)?resultList:resultList[index]
+    }
 
-    //Logic
-    const selectorProfile = {
-        personalInformation:{
-            name:"div.ph5.pb5 > div.display-flex.mt2 ul li",
-            title:"div.ph5.pb5 > div.display-flex.mt2 h2",
-            resume: 'section.pv-about-section > p'
-        },
-        experienceInformation:{
-            list : '#experience-section > ul > li',
-            groupByCompany:{
-                identify:'.pv-entity__position-group',
-                company: 'div.pv-entity__company-summary-info > h3 > span:nth-child(2)',
-                list: 'section > ul > li',
-                title: 'div > div > div > div > div > div > h3 > span:nth-child(2)',
-                date:'div > div > div > div > div > div > div > h4 > span:nth-child(2)',
-                description: '.pv-entity__description'
-            },
-            default:{
-                title: 'section > div > div > a > div.pv-entity__summary-info > h3',
-                company:'section > div > div > a > div.pv-entity__summary-info > p.pv-entity__secondary-title',
-                date: 'section > div > div > a > div.pv-entity__summary-info > div > h4.pv-entity__date-range > span:nth-child(2)',
-                description: 'section > div > div > div > p'
+
+    /* Search input text */
+    const getSearch = async ()=>{
+        const {btnSearch:search} = selectorPage
+        const input = document.querySelector(search.input)
+        input.value = search.text
+        const keycode = new KeyboardEvent('keydown', {'keyCode':13, 'which':13}); 
+        input.dispatchEvent(keycode);  
+    }
+    /* Bucle list result  */
+    const getListSearch = async ()=>{
+        const resultList = await selectorListSearchResult()
+        const profiles = []
+        for (let index = 0; index < resultList.length; index++) {
+            const item =  await selectorListSearchResult(index)
+            const data = await clickOpenPage(item)
+            profiles.push(data)
+        }
+        return profiles
+    }                                                                               
+  
+    /* Click open page detail*/
+    const personal = [];
+    const clickOpenPage = async (item)=>{
+        const {listSearch:search} = selectorPage
+        item.querySelector(search.link).click()
+        await wait(3000)
+        const data = await scrapingProfile()
+        personal.push(data)
+        window.history.back()
+        await wait(3000)
+
+        return data
+    }
+
+    const scrapingProfile = async ()=>{
+  
+        const autoscrollToElement = async function(cssSelector){
+    
+            var exists = document.querySelector(cssSelector);
+        
+            while(exists){
+                //
+                let maxScrollTop = document.body.clientHeight - window.innerHeight;
+                let elementScrollTop = document.querySelector(cssSelector).offsetHeight
+                let currentScrollTop = window.scrollY
+        
+        
+                if(maxScrollTop == currentScrollTop || elementScrollTop <= currentScrollTop)
+                    break;
+        
+                await wait(32)
+        
+                let newScrollTop = Math.min(currentScrollTop + 20, maxScrollTop);
+                window.scrollTo(0,newScrollTop)
             }
-        },
-        educationInformation:{
-            list: '#education-section > ul > li',
-            institution :'div > div > a > div.pv-entity__summary-info > div > h3',
-            career : 'div > div > a > div.pv-entity__summary-info > div > p > span:nth-child(2)',
-            date : 'div > div > a > div.pv-entity__summary-info > p > span:nth-child(2)'
+        
+            console.log('finish autoscroll to element %s', cssSelector);
+        
+            return new Promise(function(resolve){
+                resolve();
+            });
+        };
+    
+        //Logic
+        const selectorProfile = {
+            personalInformation:{
+                name:"div.ph5.pb5 > div.display-flex.mt2 ul li",
+                title:"div.ph5.pb5 > div.display-flex.mt2 h2",
+                resume: 'section.pv-about-section > p',
+                image:"div.ph5.pb5 > div.display-flex> div.pv-top-card--photo img"
+            },
+            experienceInformation:{
+                list : '#experience-section > ul > li',
+                groupByCompany:{
+                    identify:'.pv-entity__position-group',
+                    company: 'div.pv-entity__company-summary-info > h3 > span:nth-child(2)',
+                    list: 'section > ul > li',
+                    title: 'div > div > div > div > div > div > h3 > span:nth-child(2)',
+                    date:'div > div > div > div > div > div > div > h4 > span:nth-child(2)',
+                    description: '.pv-entity__description'
+                },
+                default:{
+                    title: 'section > div > div > a > div.pv-entity__summary-info > h3',
+                    company:'section > div > div > a > div.pv-entity__summary-info > p.pv-entity__secondary-title',
+                    date: 'section > div > div > a > div.pv-entity__summary-info > div > h4.pv-entity__date-range > span:nth-child(2)',
+                    description: 'section > div > div > div > p'
+                }
+            },
+            educationInformation:{
+                list: '#education-section > ul > li',
+                institution :'div > div > a > div.pv-entity__summary-info > div > h3',
+                career : 'div > div > a > div.pv-entity__summary-info > div > p > span:nth-child(2)',
+                date : 'div > div > a > div.pv-entity__summary-info > p > span:nth-child(2)'
+            }
         }
-    }
-
-    const clickOnMoreResume = async ()=>{
-        const elementMoreResume = document.getElementById('line-clamp-show-more-button')
-        if(elementMoreResume) elementMoreResume.click()
-    }
-
-    const getPersonalInformation = async ()=>{
-        const {personalInformation:selector} = selectorProfile
-        const elementNameProfile = document.querySelector(selector.name)
-        const elementNameTitle = document.querySelector(selector.title)
-        const elementResume = document.querySelector(selector.resume)
-        
-        const name = elementNameProfile?.innerText
-        const title = elementNameTitle?.innerText
-        const resume = elementResume?.innerText
-        return {name,title,resume}
-    }
-
-    const getExperienceInformation = async ()=>{
-        const {experienceInformation:selector} = selectorProfile
-        //get information
-        let experiencesRawList = document.querySelectorAll(selector.list)
-        let experiencesRawArray = Array.from(experiencesRawList)
-
-        const groupCompaniesList = experiencesRawArray.filter(el=>{
-            let groupCompanyExperience = el.querySelectorAll(selector.groupByCompany.identify)  
-            return groupCompanyExperience.length >0
-        })
-
-        const uniqueExperienceList = experiencesRawArray.filter(el=>{
-            let groupCompanyExperience = el.querySelectorAll(selector.groupByCompany.identify)  
-            return groupCompanyExperience.length ==0
-        })
-        
-        const experiences = uniqueExperienceList.map(el=>{
-            const title = el.querySelector(selector.default.title)?.innerText
-            const date = el.querySelector(selector.default.date)?.innerText
-            const company = el.querySelector(selector.default.company)?.innerText
-            const description = el.querySelector(selector.default.description)?.innerText
+    
+        const clickOnMoreResume = async ()=>{
+            const elementMoreResume = document.getElementById('line-clamp-show-more-button')
+            if(elementMoreResume) elementMoreResume.click()
+        }
+    
+        const getPersonalInformation = async ()=>{
+            const {personalInformation:selector} = selectorProfile
+            const elementNameProfile = document.querySelector(selector.name)
+            const elementNameTitle = document.querySelector(selector.title)
+            const elementResume = document.querySelector(selector.resume)
+            const elementImage = document.querySelector(selector.image)
             
-            return {title,date,company,description}
-        })
-
-        for(let i = 0; i< groupCompaniesList.length;i++){
-            const item = groupCompaniesList[i]
-            const company = item.querySelector(selector.groupByCompany.company)?.innerText
-            const itemsCompanyGroupList = item.querySelectorAll(selector.groupByCompany.list)
-            const itemsCompanyGroupArray = Array.from(itemsCompanyGroupList)
-
-            const experiencesData = itemsCompanyGroupArray.map(el=>{
-                const title = el.querySelector(selector.groupByCompany.title)?.innerText
-                const date = el.querySelector(selector.groupByCompany.date)?.innerText
-                const description = el.querySelector(selector.groupByCompany.description)?.innerText
+            const name = elementNameProfile?.innerText
+            const title = elementNameTitle?.innerText
+            const resume = elementResume?.innerText
+            const image = elementImage?.src
+            return {name,title,resume,image}
+        }
+    
+        const getExperienceInformation = async ()=>{
+            const {experienceInformation:selector} = selectorProfile
+            //get information
+            let experiencesRawList = document.querySelectorAll(selector.list)
+            let experiencesRawArray = Array.from(experiencesRawList)
+    
+            const groupCompaniesList = experiencesRawArray.filter(el=>{
+                let groupCompanyExperience = el.querySelectorAll(selector.groupByCompany.identify)  
+                return groupCompanyExperience.length >0
+            })
+    
+            const uniqueExperienceList = experiencesRawArray.filter(el=>{
+                let groupCompanyExperience = el.querySelectorAll(selector.groupByCompany.identify)  
+                return groupCompanyExperience.length ==0
+            })
+            
+            const experiences = uniqueExperienceList.map(el=>{
+                const title = el.querySelector(selector.default.title)?.innerText
+                const date = el.querySelector(selector.default.date)?.innerText
+                const company = el.querySelector(selector.default.company)?.innerText
+                const description = el.querySelector(selector.default.description)?.innerText
                 
                 return {title,date,company,description}
             })
-
-            experiences.push(...experiencesData)
+    
+            for(let i = 0; i< groupCompaniesList.length;i++){
+                const item = groupCompaniesList[i]
+                const company = item.querySelector(selector.groupByCompany.company)?.innerText
+                const itemsCompanyGroupList = item.querySelectorAll(selector.groupByCompany.list)
+                const itemsCompanyGroupArray = Array.from(itemsCompanyGroupList)
+    
+                const experiencesData = itemsCompanyGroupArray.map(el=>{
+                    const title = el.querySelector(selector.groupByCompany.title)?.innerText
+                    const date = el.querySelector(selector.groupByCompany.date)?.innerText
+                    const description = el.querySelector(selector.groupByCompany.description)?.innerText
+                    
+                    return {title,date,company,description}
+                })
+                experiences.push(...experiencesData)
+            }
+            return experiences
         }
-
-        return experiences
-    }
-
-    const getEducationInformation = async ()=>{
-        const {educationInformation:selector} = selectorProfile
-        const educationItems = document.querySelectorAll(selector.list)
-        const educationArray = Array.from(educationItems)
-        const educations = educationArray.map(el=>{
-            const institution = el.querySelector(selector.institution).innerText
-            const career = el.querySelector(selector.career).innerText
-            const date = el.querySelector(selector.date).innerText
-            return {institution,career,date}
-        })
-        return educations
-    }
-
-    const createPopup = ()=>{
-        const styleDiv = "position: fixed;z-index: 2000;width:100%; top: 0px;left: 0px;overflow: visible;display: flex;align-items: flex-end;background-color: lightgray;font-size: 10px;padding: 10px;";
-        const stylePre = "position: relative;max-height: 400px;overflow: scroll;width: 100%;"
-        const div = document.createElement('div')
-        div.id = "krowdy-message"
-        div.style = styleDiv
-
-        const pre = document.createElement('pre')
-        pre.id = "krowdy-pre"
-        pre.style = stylePre
-
-        const button = document.createElement('button')
+    
+        const getEducationInformation = async ()=>{
+            const {educationInformation:selector} = selectorProfile
+            const educationItems = document.querySelectorAll(selector.list)
+            const educationArray = Array.from(educationItems)
+            const educations = educationArray.map(el=>{
+                const institution = el.querySelector(selector.institution).innerText
+                const career = el.querySelector(selector.career).innerText
+                const date = el.querySelector(selector.date)
+                var dates = date?date.innerText:'';
+                return {institution,career,dates}
+            })
+            return educations
+        }
+    
+        await autoscrollToElement('body')
+        await clickOnMoreResume()
         
-        button.id = "krowdy-button"
-        button.style = "background: gray;border: 2px solid;padding: 8px;"
-        button.innerText ="Aceptar"
-
-        const bodyElement = document.querySelector('div.body')
+        //Scraping Complete Profile
+        const personalInformation =  await getPersonalInformation()
+        const experienceInformation = await getExperienceInformation()
+        const educationInformation = await getEducationInformation()
         
-        bodyElement.appendChild(div)
+       // pre.innerText = 'Ya tenemos las información del perfil'
+        await wait(1000)
+    
+        //Setting data to send information
+        const profile = {...personalInformation, experiences:experienceInformation, educations:educationInformation }
 
-        pre.innerText = "Estamos extrayendo la información!!!!"
-        div.appendChild(pre)
-        div.appendChild(button)
-        return {div,pre,button}
+        await wait(2000)
+        return profile;
     }
     
-    //Scroll to all information
-    const {div,pre,button} = createPopup()
+    const createNav = async ()=> {
+        const styleDiv = "background-color: #000000b8;width: 100vw; height: 100vh; position: fixed; z-index: 99;";
+        const divBlack = document.createElement('div')
+        divBlack.id = "profile-list"
+        divBlack.style = styleDiv
 
-    pre.innerText = 'Scaneando el perfil'
-    await autoscrollToElement('body')
-    await clickOnMoreResume()
-    
-    //Scraping Complete Profile
-    const personalInformation =  await getPersonalInformation()
-    const experienceInformation = await getExperienceInformation()
-    const educationInformation = await getEducationInformation()
-    
-    
-    pre.innerText = 'Ya tenemos las información del perfil'
-    await wait(1000)
+        const divWhite = document.createElement('div')
+        divWhite.style = "background-color: white;width: 500px;height: 100vh;margin: 0 auto;padding: 20px;"
+        
 
-    //Setting data to send information
-    const profile = {...personalInformation, experiences:experienceInformation, educations:educationInformation }
-    pre.innerText = JSON.stringify(profile,null,2)
 
-    button.addEventListener("click",()=>{
-        //Necesito el fetch
 
-        div.remove()
-    })
+        const bodyElement = document.querySelector('body.boot-complete')
+        var thefrist = bodyElement.firstChild
+
+        bodyElement.insertBefore(divBlack,thefrist)
+
+        const container = document.getElementById('profile-list')
+        container.appendChild(divWhite)
+
+
+    }
+    /* init search profile */
+    await createNav()
+    /*await getSearch()
+    await wait(3000)
+    const listresult = await getListSearch()
+    await wait(3000)
+    await createNav(listresult)*/
 }
-
-
 
 //Comunication
 (function(){
-chrome.runtime.onConnect.addListener(function(port) {
-    port.onMessage.addListener(function(msg) {
-      const {acction} = msg
-      console.log(acction)
-      if (acction=="scraping") scrapingProfile()
-    });
-  })})();
+    chrome.runtime.onConnect.addListener(function(port) {
+        port.onMessage.addListener(function(msg) {
+          const {acction} = msg
+          const {searchtext} = msg
+     
+          switch(acction){
+              case "search":
+                  searchProfile(searchtext)
+              break;
+          }
+        });
+})})();
+    
